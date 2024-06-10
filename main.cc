09b33da2be0d60b82d2af50ea65ffd41d023e400
yizhou.xu <yizhou.xu@sophgo.com>
2024-06-10 14:16:57 +0800

    add bmnn_utils like 84/84x/88

---
+++ main.cc
@@ -0,0 +1,11 @@
+#include "bmnn_utils1690.h"
+
+int main() {
+  const std::string modelPath =
+      "/home/xyz/projects/1690/model_trans/YOLOv5/models/BM1690/"
+      "yolov5s_v6.1_3output_int8_1b.bmodel";
+
+  auto tpu_ctx = std::make_shared<BMNNContext>(modelPath.c_str());
+  auto tpu_net = std::make_shared<BMNNNetwork>(tpu_ctx->network());
+  return 0;
+}
